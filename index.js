fbd5fa6845c5a3fd809cfee745bb144ff3028431
sumon paul <sumpaul100@gmail.com>
2023-10-19 02:37:08 +0600

    post and get method working and successfully get and post method created for brands

--- index.js
+++ index.js
@@ -24,22 +24,31 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        const brandCollections = client.db("scpElectronicsDB").collection("brands")
+
+        // getting brand apis
+        app.get("/brnads", async (req, res) => {
+            const cursor = brandCollections.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
 
         // add brands working
         app.post("/addBrand", async (req, res) => {
-            const brand = req.body;
+            const newBrand = req.body;
+            const result = await brandCollections.insertOne(newBrand)
+            res.send(result)
         })
-
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
-run().catch(console.dir);
+run().catch(console.log);
 
 app.listen(port, () => {
     console.log(`Server Listening from ${port}`)
